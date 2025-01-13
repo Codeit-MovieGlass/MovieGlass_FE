7f30fc4a489bcd3b6d34f6bab549e105d554c047
DefineXX <keaikim21@naver.com>
2025-01-14 04:24:37 +0900

    style(global): Typography의 font-size에 pxToRem 적용 (#3)

--- src/styles/theme/typography.js
+++ src/styles/theme/typography.js
@@ -1,4 +1,5 @@
 import { css } from 'styled-components';
+import pxToRem from '@utils/pxToRem';
 
 const headerBaseFontStyles = css`
   font-family: 'NEXON Lv2 Gothic';
@@ -15,65 +16,65 @@ const bodyBaseFontStyles = css`
 const headerStyles = {
   Header1: css`
     ${headerBaseFontStyles}
-    font-size: 60px;
+    font-size: ${pxToRem(60)};
     letter-spacing: 1.2px;
   `,
   Header2: css`
     ${headerBaseFontStyles}
-    font-size: 48px;
+    font-size: ${pxToRem(48)};
   `,
   Header3: css`
     ${headerBaseFontStyles}
-    font-size: 36px;
+    font-size: ${pxToRem(36)};
   `,
   Header4: css`
     ${headerBaseFontStyles}
-    font-size: 24px;
+    font-size: ${pxToRem(24)};
   `,
   Header5: css`
     ${headerBaseFontStyles}
-    font-size: 21px;
+    font-size: ${pxToRem(21)};
   `,
   Header6: css`
     ${headerBaseFontStyles}
-    font-size: 18px;
+    font-size: ${pxToRem(18)};
   `,
   Header7: css`
     ${headerBaseFontStyles}
-    font-size: 14px;
+    font-size: ${pxToRem(14)};
   `,
 };
 
 const bodyStyles = {
   Body1: css`
     ${bodyBaseFontStyles}
-    font-size: 36px;
+    font-size: ${pxToRem(36)};
   `,
   Body2: css`
     ${bodyBaseFontStyles}
-    font-size: 24px;
+    font-size: ${pxToRem(24)};
     letter-spacing: 0.72px;
   `,
   Body3: css`
     ${bodyBaseFontStyles}
-    font-size: 21px;
+    font-size: ${pxToRem(21)};
   `,
   Body4: css`
     ${bodyBaseFontStyles}
-    font-size: 18px;
+    font-size: ${pxToRem(18)};
     letter-spacing: 0.36px;
   `,
   Body5: css`
     ${bodyBaseFontStyles}
-    font-size: 16px;
+    font-size: ${pxToRem(16)};
   `,
   Body6: css`
     ${bodyBaseFontStyles}
-    font-size: 14px;
+    font-size: ${pxToRem(14)};
   `,
   Body7: css`
     ${bodyBaseFontStyles}
-    font-size: 12px;
+    font-size: ${pxToRem(12)};
   `,
 };
 
