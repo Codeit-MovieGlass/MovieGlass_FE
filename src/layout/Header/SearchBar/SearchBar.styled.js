f9ad8b365d9f35a278ac4bba29d9901bc418174e
DefineXX <keaikim21@naver.com>
2025-01-14 21:13:23 +0900

    feat(search): SearchBar 컴포넌트 UI 구현 (#5)

---
+++ src/layout/Header/SearchBar/SearchBar.styled.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import pxToRem from '@utils/pxToRem';
+
+export const SearchBarForm = styled.form`
+  width: ${pxToRem(400)};
+  height: 50px;
+  padding: 0 18px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.colors.MG_Grayscale.Gray_2};
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    width: ${pxToRem(350)};
+  }
+`;
+
+export const SearchInput = styled.input`
+  width: 90%;
+  background-color: inherit;
+
+  color: ${({ theme }) => theme.colors.MG_Grayscale.Gray_4};
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0.48px;
+
+  &::placeholder {
+    font-size: ${pxToRem(16)};
+  }
+`;
+
+export const SearchButton = styled.button`
+  width: fit-content;
+  height: fit-content;
+`;
