ee07b31b3dadda8cac9c2c06e119cc2d1120c914
Lee hwan seuk <sioo1014@naver.com>
2025-01-24 00:44:35 +0900

    feat(Auth): 로그인 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 import Layout from '@layout/Layout';
+import {Lay,LoginPage,Sign_Up_Page} from '@pages/Onboarding/AuthPage/Auth'
 
 import Home from '@pages/Home/Home';
+
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
         <Route element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
+        <Route path='/login' element={<Lay>
+          <LoginPage/></Lay>}/>
+        <Route path='/signup' element={<Lay><Sign_Up_Page/></Lay>}/>
       </Routes>
     </BrowserRouter>
   );
