8447e3045febe8a08fb40d036d7f8806a47ce850
DefineXX <keaikim21@naver.com>
2025-01-29 22:43:00 +0900

    fix(layout): 기존 Layout 컴포넌트 내 Top Container로 padding-top 값 이동

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router';
+
 import Header from './Header/Header';
+
+import * as S from './Layout.styled';
 
 const Layout = () => {
   return (
-    <>
+    <S.LayoutContainer>
       <Header />
       <Outlet />
-    </>
+    </S.LayoutContainer>
   );
 };
 
