e19163c73e2e5022892d7acc0fd5f38902a7aa54
DefineXX <keaikim21@naver.com>
2025-02-06 16:54:49 +0900

    refactor(curation): CurationSet 컴포넌트 제거 후 Home 컴포넌트에 통합

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,21 @@
-import CurationSet from '@components/Curation/CurationSet';
-import TopTenList from '@components/TopTenList/TopTenList';
 import PropTypes from 'prop-types';
+
+import TopTenList from '@components/TopTenList/TopTenList';
+import BallonSection from '@components/Balloon/BallonSection';
+import Curation from '@components/Curation/Curation';
+
+import { CurationData } from '@components/Curation/Mock/CurationData';
+
 import * as S from './Home.styled';
 
 const Home = ({ keyword }) => {
   return (
     <S.HomeContatiner>
       {keyword === '' ? <TopTenList /> : <></>}
-      <CurationSet />
+      <S.CurationWrapper>
+        <BallonSection />
+        <Curation curationTitle={CurationData.curationTitle} movieList={CurationData.movieList} />
+      </S.CurationWrapper>
     </S.HomeContatiner>
   );
 };
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
+import pxToRem from '@utils/pxToRem';
 
 export const HomeContatiner = styled.div`
   min-width: 1440px;
+`;
+
+export const CurationWrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  padding: ${pxToRem(64)} ${pxToRem(64)};
 `;
