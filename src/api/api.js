48399346f052ed4e00af51ea38b2ed57505d33d4
DefineXX <keaikim21@naver.com>
2025-02-13 17:13:17 +0900

    feat(axios): Axios Interceptor 임시 설정 (#37)

--- src/api/api.js
+++ src/api/api.js
@@ -7,4 +7,49 @@ const api = axios.create({
   },
 });
 
+// Request Interceptor
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Response Interceptor (401 Unauthorized 처리)
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true; // API 요청 재시도 Flag
+
+      try {
+        // 토큰 재발급 요청 로직 수정 필요
+        const newToken = await api.post('/auth/refresh-token');
+        console.log(newToken);
+
+        localStorage.setItem('accessToken', newToken.data.accessToken);
+        localStorage.setItem('refreshToken', newToken.data.refreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newToken.data.accessToken}`;
+        return api(originalRequest); // 실패한 요청 재시도
+      } catch (error) {
+        console.error('Error refreshing token:', error);
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/login';
+      }
+
+      return Promise.reject(error);
+    }
+  }
+);
+
 export default api;
