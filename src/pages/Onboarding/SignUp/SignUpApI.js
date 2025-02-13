d610cc30083cf5575449d667d39548cf561212a0
Lee hwan seuk <sioo1014@naver.com>
2025-02-13 09:53:38 +0900

    fix(signup): 회원가입 시 이메일,닉네임 중복검사 및 알림창 구현(#37)

--- src/pages/Onboarding/SignUp/SignUpApI.js
+++ src/pages/Onboarding/SignUp/SignUpApI.js
@@ -15,17 +15,12 @@ export function useSignup() {
       const response = await axios.post('', formData);
       console.log(response);
 
-      if (response.data.isSuccess) {
-        console.log(response.result.message)
-        alert(`회원가입에 성공하셨습니다!`); 
-        if(onSuccess) onSuccess();
-      } else {
-        console.log(response.status)
-        alert(`회원가입 실패: ${response.data.message}`);//이메일 중복인지 닉네임 중복인지 구분분
-      }
+      alert(`회원가입에 성공하셨습니다!`); 
+      if(onSuccess) onSuccess();
+      
     } catch (error) {
-      console.error("서버 오류 발생:", error);
-      alert("회원가입 중 오류가 발생했습니다. 다시 시도해주세요.");
+      console.error("서버 오류 발생:", error.response.data);
+      alert(error.response.data.result.errorMessage);
     } 
   };
 
