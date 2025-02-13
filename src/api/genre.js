fe98354c022180e58e42b901b4b830d6a107a313
Lee hwan seuk <sioo1014@naver.com>
2025-02-13 15:11:48 +0900

    fix(genre): 장르 선택 API 수정(#40)

--- src/api/genre.js
+++ src/api/genre.js
@@ -1,24 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const handleNext = async (selectedGenres, genres, navigate) => {
   if (selectedGenres.length === 2) {
     const selectedGenreNames = genres
       .filter((genre) => selectedGenres.includes(genre.id))
-      .map((genre) => genre.name)
+      .map((genre) => genre.name.replace(/\//g, ','))
       .join(',');
 
     console.log('보내는 장르:',selectedGenreNames);
     
 
     try {
-      const response = await axios.get('http://3.35.55.17/moviechoice/genre', {
+      const response = await axios.get('', {
         params: { genre: selectedGenreNames },
       });
 
       console.log('백엔드 응답 데이터:', response);
 
-      const movies = response.data;
-      navigate('/select/genre', { state: { movies } });
+      const movies = response.data.data;
+      console.log('받아온 영화 데이터:',movies);
+
+      navigate('/select/moive', { state: { movies } });
     } catch (error) {
       console.error('영화 데이터 가져오기 실패:', error);
     }
