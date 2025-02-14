f51352cdba3c4fdad641e5f49a0ec9fc831538e6
DefineXX <keaikim21@naver.com>
2025-02-14 15:02:50 +0900

    feat(modal): 영화 상세 모달 내 정보 페이지 UI 구현 (#17)

--- src/pages/MovieModal/MovieModal.jsx
+++ src/pages/MovieModal/MovieModal.jsx
@@ -1,16 +1,40 @@
+import { useState } from 'react';
+
 import Rating from '@components/Rating/Rating';
 import Heart from '@components/Heart/Heart';
 import ViewCount from '@components/ViewCount/ViewCount';
-
-import * as S from './MovieModal.styled';
 import OTTLink from '@components/OTTLink/OTTLink';
 
-const MovieModal = () => {
-  const movieInfos = {
+import * as S from './MovieModal.styled';
+
+const movieInfos = {
+  title: '탑건: 매버릭',
+  rating: 4.5,
+  like: true,
+  viewCount: 1000,
+  infos: {
     title: '탑건: 매버릭',
-    rating: 4.5,
-    like: true,
-    viewCount: 1000,
+    genre: ['액션', '드라마'],
+    keyword: ['힝공', '전투기', '파일럿'],
+    director: '조셉 코신스키',
+    actor:
+      '톰 크루즈, 마일스 텔러, 제니퍼 코넬리,존 햄, 글렌 포웰, 루이스 풀먼,에드 해리스, 발 킬머',
+    releaseDate: '2022-05-25',
+    runtime: '2시간 10분',
+    ageLimit: '12',
+    synopsis:
+      '최고의 파일럿이자 전설적인 에이스, 매버릭(톰 크루즈) 은 수십 년간 최전방에서 활약했지만, 여전히 규칙을 깨며 자신만의 방식으로 살아간다. 그는 탑건(TOPGUN) 출신의 젊은 조종사들을 가르치는 훈련 교관으로 복귀하지만, 그 중 한 명이 과거 전우의 아들이자 자신을 원망하는 루스터(마일스 텔러) 이다. 한편, 국가를 위협하는 새로운 미션이 주어지고, 이들은 성공 확률이 극히 낮은 작전에 투입된다.전투기를 타고 운명을 건 미션에 나서는 매버릭과 젊은 조종사들, 과연 그들은 불가능을 가능으로 만들 수 있을 것인가?',
+  },
+};
+
+const MovieModal = () => {
+  // 선택된 탭 정보 상태 관리
+  const infoTabs = ['정보', '관람평'];
+
+  const [selectedTab, setSelectedTab] = useState('정보');
+
+  const handleTabClick = (tabName) => {
+    setSelectedTab(tabName);
   };
 
   return (
@@ -41,6 +65,74 @@ const MovieModal = () => {
         {/* OTT Link */}
         <OTTLink />
       </S.MetaDataSection>
+
+      {/* 정보, 관람평 선택 탭 */}
+      <S.InfoTabSection>
+        <S.InfoTabList>
+          {infoTabs.map((tabName) => (
+            <S.InfoTabItem key={tabName}>
+              <S.InfoTabButton
+                onClick={() => handleTabClick(tabName)}
+                $isSelected={selectedTab === tabName}
+              >
+                {tabName}
+              </S.InfoTabButton>
+            </S.InfoTabItem>
+          ))}
+        </S.InfoTabList>
+      </S.InfoTabSection>
+
+      {/* 영화 정보 표시 영역 */}
+      <S.MovieInfoSection>
+        {/* 영화 제목, 러닝타임, 개봉일 정보 */}
+        <S.MovieTitleInfoSection>
+          <S.MovieTitle>{movieInfos.title}</S.MovieTitle>
+          <S.MovieRunningTimeReleaseDateContainer>
+            <S.MovieRunningTime>러닝타임 {movieInfos.infos.runtime}</S.MovieRunningTime>
+            <S.MovieReleaseDate>{movieInfos.infos.releaseDate}</S.MovieReleaseDate>
+          </S.MovieRunningTimeReleaseDateContainer>
+        </S.MovieTitleInfoSection>
+
+        {/* 출연진, 장르, 등급, 키워드 */}
+        <S.CastGenreSection>
+          {/* 출연진 */}
+          <S.CastContainer>
+            <S.CastTitle>출연진</S.CastTitle>
+            <S.CastActors>{movieInfos.infos.actor}</S.CastActors>
+          </S.CastContainer>
+
+          {/* 관람 등급, 장르 및 키워드 */}
+          <S.AgeRatingGenreContainer>
+            <S.AgeRatingIcon
+              src={`/AgeRating/${movieInfos.infos.ageLimit}.svg`}
+              alt={`${movieInfos.infos.ageLimit}세 이상 관람가`}
+            />
+            <S.GenreKeywordContainer>
+              <S.GenreKeywordList>
+                {movieInfos.infos.genre.map((genre) => (
+                  <S.GenreKeywordItem key={genre}>
+                    <S.GenreInfo>#{genre}</S.GenreInfo>
+                  </S.GenreKeywordItem>
+                ))}
+              </S.GenreKeywordList>
+
+              <S.GenreKeywordList>
+                {movieInfos.infos.keyword.map((keyword) => (
+                  <S.GenreKeywordItem key={keyword}>
+                    <S.KeywordInfo>#{keyword}</S.KeywordInfo>
+                  </S.GenreKeywordItem>
+                ))}
+              </S.GenreKeywordList>
+            </S.GenreKeywordContainer>
+          </S.AgeRatingGenreContainer>
+        </S.CastGenreSection>
+
+        {/* 영화 줄거리 */}
+        <S.SynopsisSection>
+          <S.SynopsisTitle>줄거리</S.SynopsisTitle>
+          <S.SynopsisContent>{movieInfos.infos.synopsis}</S.SynopsisContent>
+        </S.SynopsisSection>
+      </S.MovieInfoSection>
     </S.MovieModalContainer>
   );
 };
--- src/pages/MovieModal/MovieModal.styled.js
+++ src/pages/MovieModal/MovieModal.styled.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import pxToRem from '@utils/pxToRem';
 
 export const MovieModalContainer = styled.div`
-  width: 35%;
+  width: ${pxToRem(1200)};
   height: fit-content;
 
   display: flex;
@@ -21,8 +21,6 @@ export const MovieModalContainer = styled.div`
 export const MovieTrailerSection = styled.section`
   width: 100%;
   height: fit-content;
-
-  border-radius: 16px;
 `;
 
 // Youtube Embed 영상으로 추후 대체
@@ -85,4 +83,235 @@ export const LikeViewCountContainer = styled.div`
   display: flex;
   align-items: center;
   gap: ${pxToRem(36)};
+`;
+
+// 정보, 관람평 선택 탭
+export const InfoTabSection = styled.section`
+  width: 100%;
+  height: fit-content;
+  padding: ${pxToRem(24)} ${pxToRem(48)};
+`;
+
+export const InfoTabList = styled.ul`
+  width: fit-content;
+  height: fit-content;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const InfoTabItem = styled.li`
+  width: fit-content;
+  height: fit-content;
+`;
+
+export const InfoTabButton = styled.button`
+  position: relative;
+
+  width: ${pxToRem(128)};
+  height: fit-content;
+  padding: ${pxToRem(12)} ${pxToRem(32)};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${({ theme }) => theme.fontStyles.Body2}
+
+  ${({ $isSelected }) =>
+    $isSelected
+      ? css`
+          color: ${({ theme }) => theme.colors.MG_Signature.Primary};
+          transition: color 0.15s ease-out;
+        `
+      : css`
+          color: ${({ theme }) => theme.colors.MG_Grayscale.Gray_3};
+          transition: color 0.15s ease-out;
+        `}
+
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background-color: ${({ theme }) => theme.colors.MG_Grayscale.Gray_3};
+
+    ${({ $isSelected }) =>
+      $isSelected
+        ? css`
+            background-color: ${({ theme }) => theme.colors.MG_Signature.Primary};
+            transition: background-color 0.15s ease-out;
+          `
+        : css`
+            background-color: ${({ theme }) => theme.colors.MG_Grayscale.Gray_3};
+            transition: background-color 0.15s ease-out;
+          `}
+  }
+`;
+
+// 영화 정보 표시 영역
+export const MovieInfoSection = styled.section`
+  width: 100%;
+  height: fit-content;
+  padding: ${pxToRem(32)} ${pxToRem(64)} ${pxToRem(64)};
+
+  display: flex;
+  flex-direction: column;
+  gap: ${pxToRem(48)};
+`;
+
+// 영화 제목, 러닝타임, 개봉일 정보
+export const MovieTitleInfoSection = styled.section`
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const MovieTitle = styled.span`
+  ${({ theme }) => theme.fontStyles.Header3}
+  color: ${({ theme }) => theme.colors.MG_Grayscale.White};
+
+  white-space: nowrap;
+`;
+
+export const MovieRunningTimeReleaseDateContainer = styled.div`
+  width: fit-content;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: ${pxToRem(8)};
+`;
+
+export const MovieRunningTime = styled.span`
+  ${({ theme }) => theme.fontStyles.Body2}
+  color: ${({ theme }) => theme.colors.MG_Grayscale.White};
+`;
+
+export const MovieReleaseDate = styled.span`
+  ${({ theme }) => theme.fontStyles.Body2}
+  color: ${({ theme }) => theme.colors.MG_Grayscale.Gray_3};
+`;
+
+// 출연진, 장르, 등급, 키워드
+export const CastGenreSection = styled.section`
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const CastContainer = styled.div`
+  width: 40%;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  gap: ${pxToRem(28)};
+`;
+
+const castSynopsisTitle = css`
+  ${({ theme }) => theme.fontStyles.Body2}
+  color: ${({ theme }) => theme.colors.MG_Grayscale.Gray_3};
+
+  white-space: nowrap;
+`;
+
+export const CastTitle = styled.span`
+  ${castSynopsisTitle}
+`;
+
+export const CastActors = styled.span`
+  width: 100%;
+  height: fit-content;
+
+  ${({ theme }) => theme.fontStyles.Body2}
+  line-height: 1.3;
+  color: ${({ theme }) => theme.colors.MG_Grayscale.White};
+`;
+
+// 관람 등급, 장르, 키워드
+export const AgeRatingGenreContainer = styled.div`
+  width: fit-content;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: ${pxToRem(28)};
+`;
+
+export const AgeRatingIcon = styled.img`
+  width: ${pxToRem(48)};
+  height: ${pxToRem(48)};
+`;
+
+export const GenreKeywordContainer = styled.div`
+  width: fit-content;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: ${pxToRem(8)};
+`;
+
+export const GenreKeywordList = styled.ul`
+  width: fit-content;
+  height: fit-content;
+
+  display: flex;
+  align-items: center;
+  gap: ${pxToRem(8)};
+`;
+
+export const GenreKeywordItem = styled.li`
+  width: fit-content;
+  height: fit-content;
+`;
+
+const genreKeywordStyles = css`
+  ${({ theme }) => theme.fontStyles.Body2}
+  color: ${({ theme }) => theme.colors.MG_Signature.Light1};
+
+  white-space: nowrap;
+`;
+
+export const GenreInfo = styled.span`
+  ${genreKeywordStyles}
+`;
+
+export const KeywordInfo = styled.span`
+  ${genreKeywordStyles}
+`;
+
+// 영화 줄거리
+export const SynopsisSection = styled.section`
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  gap: ${pxToRem(28)};
+`;
+
+export const SynopsisTitle = styled.span`
+  ${castSynopsisTitle}
+`;
+
+export const SynopsisContent = styled.p`
+  width: 100%;
+  height: fit-content;
+
+  ${({ theme }) => theme.fontStyles.Body2}
+  color: ${({ theme }) => theme.colors.MG_Grayscale.White};
 `;
