6ffbc67768fc860d0aebbdae0007182fd94331fb
DefineXX <keaikim21@naver.com>
2025-02-14 18:12:14 +0900

    chore: import문 순서 수정

--- src/components/OTTLink/OTTLink.styled.js
+++ src/components/OTTLink/OTTLink.styled.js
@@ -1,5 +1,5 @@
-import pxToRem from '@utils/pxToRem';
 import styled from 'styled-components';
+import pxToRem from '@utils/pxToRem';
 
 export const OTTLinkContainer = styled.div`
   width: fit-content;
