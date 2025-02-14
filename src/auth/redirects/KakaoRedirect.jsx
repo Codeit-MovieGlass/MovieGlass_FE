c69b8c21427ab8a9dca0b14f132051b9ec7e09d7
DefineXX <keaikim21@naver.com>
2025-02-15 01:16:45 +0900

    feat(auth): postAuthCodeToServer API 함수 분리 및 로그아웃 API 로직 구현 (#65)

--- src/auth/redirects/KakaoRedirect.jsx
+++ src/auth/redirects/KakaoRedirect.jsx
@@ -3,9 +3,10 @@
 //   - 리다이렉트 주소
 //   - access 토큰 만료시 refresh 토큰을 활용해서 새로운 access 토큰 재발급 로직
 
-import api from '@api/api';
 import { useEffect, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router';
+
+import { postAuthCodeToServer } from '@auth/utils/authHelper';
 
 import LoadingFullScreen from '@pages/LoadingFullScreen/LoadingFullScreen';
 
@@ -24,38 +25,27 @@ const KakaoRedirect = () => {
     if (KAKAO_AUTH_CODE) {
       console.log('Kakao Authorization Code: ', KAKAO_AUTH_CODE);
 
-      const postAuthCodeToserver = async () => {
+      const handleKakaoAuth = async () => {
         try {
-          const response = await api.post('/auth/kakao', {
-            code: KAKAO_AUTH_CODE,
-          });
+          const response = await postAuthCodeToServer(KAKAO_AUTH_CODE);
 
-          console.log(response.data);
-
-          if (response.data.isSuccess) {
-            const userId = response.data.result.userId;
-            localStorage.setItem('userId', userId);
-
-            if (response.status === 201) {
-              // 최초 회원가입
-
-              navigate('/select/genre');
-            } else if (response.status === 200) {
-              // 기존 로그인
+          switch (response.status) {
+            case 200:
               localStorage.setItem('accessToken', response.data.accessToken);
-              localStorage.setItem('refreshToken', response.data.refreshToken);
               navigate('/');
-            }
-          } else {
-            console.warn('Authorization Code POST fail');
-            navigate('/login');
+              break;
+            case 201:
+              navigate('/select/genre');
+              break;
+            default:
+              console.error('Kakao Login Failed: ', response.status);
           }
         } catch (error) {
-          console.error('Fetching access token failed: ', error);
+          console.error('Kakao Login failed: ', error);
         }
       };
 
-      postAuthCodeToserver();
+      handleKakaoAuth();
     }
   }, [KAKAO_AUTH_CODE, navigate]);
 
